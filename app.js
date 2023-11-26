995c8ef1ae2ffb4be449d45243b832c9c1580277
Priangsu Bose <priangsubose18@gmail.com>
2023-11-27 04:42:56 +0530

    1:18

--- app.js
+++ app.js
@@ -107,14 +107,65 @@ const songs = [
       'Dunny 82K<br><div class="subtitle">AP Dhillon, Gurinder Gill, Shinda Kahlon</div>',
     poster: "img/20.jpg",
   },
+  
 ];
 
 Array.from(document.getElementsByClassName("songItem")).forEach((e, i) => {
   e.getElementsByTagName("img")[0].src = songs[i].poster;
   e.getElementsByTagName("h5")[0].innerHTML = songs[i].songName;
 });
+
+// search data start
+let search_results = document.getElementsByClassName('search_results');
+
+songs.forEach(element => {
+  const (id, songName, poster) = element;
+  //console.log(poster);
+  let card = document.createElement('a');
+  card.classList.add('card');
+  card.href = "#" + id;
+  card.innerHTML = `
+  <img src="${poster}" alt="">
+                <div class="content">
+                    ${songName} 
+                </div>
+  `;
+  search_results.appendChild(card);
+});
+
+
+let input = document.getElementsByTagName('input')[0];
+
+input.addEventListener('keyup', ()=>{
+  let input_value = input.value.toUpperCase();
+  let items = search_results.getElementsByTagName('a');
+
+  for (let index = 0; index < items.length; index++) {
+      let as = items[index].getElementsByClassName('content')[0];
+      let text_value = as.textContent || as.innerHTML;
+
+      if (text_value.toUpperCase().indexOf(input_value) > -1) {
+        items[index].style.display = "flex"; 
+      } else {
+        items[index].style.display = "none";
+        
+      }
+
+      if (input.value ==0) {
+        search_results.style.display = "none"; 
+      } else {
+        search_results.style.display = "none";
+        
+      }
+
+  }
+})
+// search data end
+
+
 let masterPlay = document.getElementById("masterPlay");
 let wave = document.getElementById("wave");
+
 masterPlay.addEventListener("click", () => {
   if (music.paused || music.currentTime <= 0) {
     music.play();
@@ -457,3 +508,4 @@ music.addEventListener("ended", () => {
       break;
   }
 });
+
